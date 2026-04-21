335f800824dd710fabd0d671566bf9476414edb8
jloyd-c <jhonloydcanaria4@gmail.com>
2026-04-21 12:47:50 +0800

    Sprint 5: Update Patient functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from patient import Patient
-from utils import add_patient, view_all_patients, search_patient
+from utils import add_patient, view_all_patients, search_patient, update_patient
 
 patients = []
 
@@ -10,8 +10,15 @@ def gender():
 def menu():
     print("1. Add Patient")
     print("2. View Patients")
-    print("3. Search Patient")
-    print("4. Exit")
+    print("3. Search Patients")
+    print("4. Update Patients")
+    print("5. Exit")
+
+def update_choices():
+    print("1. Update Name")
+    print("2. Update Age")
+    print("3. Update Gender")
+    print("4. Update Contact Number")
 
 while True:
     menu()
@@ -56,5 +63,39 @@ while True:
         search_patient(patients, search)
 
     elif user_input == 4:
+        view_all_patients(patients)
+        if len(patients) == 0:
+             continue
+        update = input("Enter Patient ID: ")
+        update_choices()
+        choice_option = int(input("Enter Update Choices: "))
+        if choice_option == 1:
+            name = input("Enter Patient Name: ")
+        elif choice_option == 2:
+            try:
+                age = int(input("Enter Patient Age: "))
+            except ValueError:
+                    print("Invalid Input Please Enter Number!")
+                    continue
+        elif choice_option == 3:
+            gender()
+            try:
+                user_gender = int(input("Choose Gender: "))
+            except ValueError:
+                    print("Invalid Input Please Enter Number!")
+                    continue
+            if user_gender == 1:
+                    user_gender = "Male"
+            elif user_gender == 2:
+                    user_gender = "Female"
+        elif choice_option == 4:
+            try:
+                contact_number = int(input("Enter Patient Contact Number: "))
+            except ValueError:
+                    print("Invalid Input Please Enter Number!")
+                    continue
+        update_patient(patients, update, name, age, user_gender, contact_number, choice_option)
+
+    elif user_input == 5:
         print("Goodbye!")
         break
--- utils.py
+++ utils.py
@@ -20,3 +20,18 @@ def search_patient(patients, search):
             found = True
     if not found:
         print("Not Found")
+
+def update_patient(patients, update, name, age, gender, contact_number, choice_option):
+    for index, patient in enumerate(patients):
+        if update == patient.patient_id:
+            if choice_option == 1:
+                patients[index].name = name
+            elif choice_option == 2:
+                patients[index].age = age
+            elif choice_option == 3:
+                patients[index].gender = gender
+            elif choice_option == 4:
+                patients[index].contact_number = contact_number
+            print(f"Patient: {patient.patient_id} updated info")
+
+        
